b9b6331aebd730ed1556082f321e3e793ef2f3a4
Tanmay Pradhan <78547592+tanmaypradhan4112@users.noreply.github.com>
2021-11-28 21:46:43 +0530

    Add files via upload

---
+++ game_stats.py
@@ -0,0 +1,18 @@
+#Statistics of Alien Games.
+class GameStats():
+    """Track statistics for Alien Invasion."""
+    def __init__(self, ai_settings):
+        """Initialize statistics."""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+        #start Alien Invasion in an active stats
+        self.game_active = False
+        self.high_score = 0
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        
+        self.level = 1
+
